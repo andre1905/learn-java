d62a4b5d670107c445ad33ad820a8660622ab587
rafael <rafael@190.142.219.123>
2018-08-11 08:59:51 -0400

    java comint

---
+++ src/beersong/Beersong.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beersong;
+
+/**
+ *
+ * @author rafael
+ */
+public class Beersong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+        
+        while (beerNum > 0 ) {  
+            
+            if (beerNum ==1) {
+                word = "bottle"; //singular, as in One bottle
+            }
+            System.out.println (beerNum + " " + word + "of beer on the wall");
+            System.out.println (beerNum + " " + word + "of beer.");
+            System.out.println ("Take one down.");
+            System.out.println ("Pass it around.");
+            beerNum = beerNum - 1;
+            
+            if (beerNum > 0 ) {
+                System.out.println (beerNum + " " + word + "of beer on the wall");
+            } else {
+                System.out.println ("No more Bottles of beer on the wall");
+            }
+          }
+        }
+     }
